f46bad00652f06ce11fb02254e5ed017685254b0
Gideon Paul <gidzpaul95@gmail.com>
2017-11-03 17:26:39 -0400

    Modified the write system call

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -4,50 +4,53 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
-#include "threads/vaddr.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+#include <list.h>
+#include <threads/thread.h>
 static void syscall_handler (struct intr_frame *);
+
+//Declaring the file decriptors for file system calls
+//static struct file_list;
+
+struct file_info
+{
+	struct list_elem elem;
+	struct file *fileval;
+	int handle;
+};
+
 
 void halt (void);
 int write (int fd, const void * buffer,unsigned size);
+//TODO : a function to validate user pointer,string and a buffer
+//bool validate_uaddr();
 
-//Function to validate user pointer,string and a buffer
-bool validate_address(const void *vaddr)
-{
-    if(is_user_vaddr(vaddr))
-    {
-      return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+//Methods to copy from user to kernel memory
 
-/*The following methods  are taken from pintos documentation guide*/
-
-/* Reads a byte at user virtual address UADDR.
+/*Reads a byte at user virtual address UADDR.
 UADDR must be below PHYS_BASE.
 Returns the byte value if successful, -1 if a segfault
 occurred. */
 static int
 get_user (const uint8_t *uaddr)
 {
-  int result;
-  asm ("movl $1f, %0; movzbl %1, %0; 1:"
-  : "=&a" (result) : "m" (*uaddr));
-  return result;
+int result;
+asm ("movl $1f, %0; movzbl %1, %0; 1:"
+: "=&a" (result) : "m" (*uaddr));
+return result;
 }
 
-/* Writes BYTE to user address UDST.
+/*Writes BYTE to user address UDST.
 UDST must be below PHYS_BASE.
 Returns true if successful, false if a segfault occurred. */
 static bool
 put_user (uint8_t *udst, uint8_t byte)
 {
-  int error_code;
-  asm ("movl $1f, %0; movb %b2, %1; 1:"
-  : "=&a" (error_code), "=m" (*udst) : "q" (byte));
-  return error_code != -1;
+int error_code;
+asm ("movl $1f, %0; movb %b2, %1; 1:"
+: "=&a" (error_code), "=m" (*udst) : "q" (byte));
+return error_code != -1;
 }
 
 void
@@ -59,56 +62,95 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  //Need to get the interrupt code here and redirect to a particular
-  ASSERT(validate_address(f->esp))
+  //need to get the interrupt code here and redirect to a particular
   int const syscall_no = *((int * ) f->esp);
-  printf ("System call : %d\n",syscall_no);
+  int args[3];
+  // need to distpach it to the handler basis the argument number
+
+  //printf ("system call! with number%d\n",syscall_no);
   switch(syscall_no)
   {
   	case SYS_HALT :
 		{
-      shutdown_power_off();
+			halt();
 		}
 		break;
-
   	case SYS_EXIT :
-  	{
-      //Simplest implementation of exit
+  		{
+  		//printf("Inside exit");
   		thread_exit();
+  		// need to have process_terminate for it
+  		}
+  		break;
+  	case SYS_EXEC :
+  		{
 
-  	}
-  	break;
 
+  		}
+  		break;
+  	case SYS_WAIT :
+  		{
+
+  		}
+  		break;
+  	case SYS_CREATE :
+  		{
+
+  		}
+  		break;
+  	case SYS_REMOVE :
+  		{
+
+
+  		}
+  		break;
+  	case SYS_OPEN :
+  		{
+
+  		}
+  		break;
+  	case SYS_FILESIZE :
+  		{
+
+  		}
+  		break;
+  	case SYS_READ :
+  		{
+
+  		}
+  		break;
   	case SYS_WRITE :
-  	{
-      //int write (int fd,const void *buffer,unsigned size)
-    	//int fd = *((int * ) (f->esp + 1)); 
-      //Should have 1 for printf
+  		{// this has a definition of int write (int fd,const void *buffer,unsigned size)
+  			//int fd = *((int * ) (f->esp + 1)); // should have 1 for printf
+  			int fd = *((int *)(f->esp) + 1);
+  			//void const *buffer = ((void *)(f->esp) + 2);
+  			void const * buffer = (char*)(*((uint32_t*)f->esp + 2));
+  			unsigned size = *((unsigned *)(f->esp) + 3);
 
-      int fd = *((int *)(f->esp) + 1);
-      printf("The fd is %d\n",fd);
-  		void *buffer = ((void *)(f->esp) + 2);
-  		int size = *((int *)(f->esp) + 3);
+  			f->eax = write(fd,buffer,size);
+  		}
+  		break;
+  	case SYS_SEEK :
+  		{
 
-  		write(fd,buffer,size);
-  		return size;
-  	}
-  	break;
-    case 6:
-    {
-      hex_dump(0xc0000000, f->esp, sizeof(char) * 60, true);
-    }
-    break;
+  		}
+  		break;
+  	case SYS_TELL :
+  		{
 
-    default :
-    	thread_exit ();
+  		}
+  		break;
+  	case SYS_CLOSE :
+  		{
+
+  		}
+  		break;
+  default :
+  	thread_exit ();
 	  }
 
 }
 
-/*In case we need to do more stuff before halting system, use this method
-  instead of directly calling shutdown_power_off()
-*/
 void halt(void)
 {
 	shutdown_power_off();
@@ -118,12 +160,12 @@ int write(int fd, const void *buffer,unsigned size)
 {
 	if (fd == 1)
 	{
-		printf("Inside the console output\n");
-		//putbuf(buffer,size);
+		// putbuf has char type buffer and size_t as size
+		putbuf(buffer,size);
 	}
 	else
 	{
-		//Write for a specific code
+		// write for a specific file
 	}
 
 	return size;
