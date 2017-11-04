8c5414a66c2c904d59f6232475c7a1d2e12a1046
Aditya Kohli <adityako@buffalo.edu>
2017-11-04 09:11:04 -0400

    Changes in thread structures and proces_files

--- threads/thread.h
+++ threads/thread.h
@@ -89,9 +89,11 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+    int handle;                         /* Handle for handling the file; initialized to 2*/
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    struct list process_files;          /* List to hold the process files*/
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
